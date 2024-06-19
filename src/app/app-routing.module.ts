3992e3cd1c17b447001f306f23b93b6dd5bc0506
renan-augusto <renan_acm1992@hotmail.com>
2024-06-19 07:12:30 -0300

    construction(home) - home page construction initialized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
+
+// import { NgModule } from '@angularcore';
+// import { Routes, RouterModule } from '@angularrouter';
+// import { HomePageComponent } from '.home-pagehome-page.component';
+
+
+// const routes Routes = [
+//   { path '', component HomePageComponent }
+// ];
+
+// @NgModule({
+//   imports [RouterModule.forRoot(routes)],
+//   exports [RouterModule]
+// })
+// export class AppRoutingModule { }
